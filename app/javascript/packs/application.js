0e912436f010c5240101bbef1cb1fae69bfe99fc
Ryo Takahashi <ryogiftjp@gmail.com>
2021-07-14 22:44:10 +0900

    Delete sprockets

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+const images = require.context("../images", true)
+const imagePath = name => images(name, true)
