242922168be57edd3a23543091ee71a15b2448d4
Ryo Takahashi <ryogiftjp@gmail.com>
2021-07-14 22:58:42 +0900

    Exclusion generators assets

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,8 @@ module OnlyWebpacker
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.assets false
+    end
   end
 end
